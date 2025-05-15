b101c5b5028413dc0912f5f307e0d0985fb37d89
zk0008 <zekangkhoo@gmail.com>
2025-05-15 10:19:41 +0800

    feat-background

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import Background from "./Background";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div style={{ position: "relative", height: "100vh" }}>
+    <div className="relative min-h-screen">
       {/* Background */}
       <Background />
       {/* Main Layout Content (Header, Footer, and Dynamic Content) */}
